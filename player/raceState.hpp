3b4f401dbd03505749e349f5f4993338a3b003ba
Shunta Watanabe <wshun0327@gmail.com>
2017-11-02 13:55:55 +0900

    racestateにコメントを追加

--- player/raceState.hpp
+++ player/raceState.hpp
@@ -5,48 +5,66 @@
 using namespace std;
 
 struct IntVec {
-  int x, y;
-  IntVec(int x = 0, int y = 0): x(x), y(y) {};
-  inline IntVec operator+(const IntVec &another) const {
-    return IntVec(x+another.x, y+another.y);
-  }
-  inline bool operator==(const IntVec &another) const {
-    return x == another.x && y == another.y;
-  }
-  inline bool operator!=(const IntVec &another) const {
-    return !(*this == another);
-  }
-  inline bool operator<(const IntVec &another) const {
-    return y != another.y ? y < another.y : x < another.x;
-  }
+    int x, y;
+
+    explicit IntVec(int x = 0, int y = 0) : x(x), y(y) {};
+
+    inline IntVec operator+(const IntVec &another) const {
+        return IntVec(x + another.x, y + another.y);
+    }
+
+    inline bool operator==(const IntVec &another) const {
+        return x == another.x && y == another.y;
+    }
+
+    inline bool operator!=(const IntVec &another) const {
+        return !(*this == another);
+    }
+
+    inline bool operator<(const IntVec &another) const {
+        return y != another.y ? y < another.y : x < another.x;
+    }
 };
 
 typedef IntVec Point;
 
+// 直線
 struct LineSegment {
-  Point p1, p2;
-  LineSegment() {};
-  LineSegment(Point p1, Point p2): p1(p1), p2(p2) {};
-  bool goesThru(const Point &p) const;
-  bool intersects(const LineSegment &l) const;
+    Point p1, p2;
+
+    LineSegment() = default;
+
+    LineSegment(Point p1, Point p2) : p1(p1), p2(p2) {};
+
+    bool goesThru(const Point &p) const;
+
+    bool intersects(const LineSegment &l) const;
 };
 
-enum ObstState { UNKNOWN, OBSTACLE, NONE };
+// 座標の状態
+enum ObstState {
+    UNKNOWN, OBSTACLE, NONE
+};
 
 struct Course {
-  int width, length;
-  int vision;
-  int thinkTime, stepLimit;
-  vector <vector <ObstState>> obstacle;
-  bool obstacled(Point &from, Point &to) const;
-  Course(istream &in);
+    unsigned int length;
+    unsigned int width;
+    int vision;
+    int thinkTime, stepLimit;
+    vector<vector<ObstState>> obstacle;
+
+    bool obstacled(Point &from, Point &to) const;
+
+    explicit Course(istream &in);
 };
 
 struct RaceState {
-  int step;
-  int timeLeft;
-  Point position, oppPosition;
-  IntVec velocity, oppVelocity;
-  RaceState(istream &in, Course &course);
-  bool collides(const LineSegment &m) const;
+    int step;
+    int timeLeft;
+    Point position, oppPosition;
+    IntVec velocity, oppVelocity;
+
+    RaceState(istream &in, Course &course);
+
+    bool collides(const LineSegment &m) const;
 };
