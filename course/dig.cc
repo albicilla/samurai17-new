1f9dd140ce59c7d7b30b23c4814259bfc435d8e5
albicilla <t.albicilla@gmail.com>
2017-11-30 10:45:52 +0900

    dig.cc にコマンドライン引数オプション追加

--- course/dig.cc
+++ course/dig.cc
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <unistd.h>
 using namespace std;
 #define FOR(i,a,b) for(int i=a;i<b;i++)
 #define rep(i,b) FOR(i,0,b)
@@ -14,35 +15,89 @@ using ll = long long;
 const ll mod = LLONG_MAX;
 
 //高さ　横
-int maze[110][20];
+int height=100,width=15;
+//上を選ぶ確率
+int choice_up=70;
+int thinkTime = 20000;
+int repeat=3;
+int maze[1123456][1123456];
 string obstacle;
 
 void dig(int x,int y,int len){
   maze[y][x]=0;
   if(y==len)return;
   //srand((unsigned int)time(NULL));
-  //0左 1上 2上　3右
+  //choice_upの確率で上に低いとエラーが出るので注意
   random_device rnd;     // 非決定的な乱数生成器
-  int togo=rnd()%4;
-  if(togo==0){
+  int togo=rnd()%100+1;
+  if(togo<=choice_up){
+    //cout<<"up"<<endl;
+    if(len==0)dig(x,y-1,len);
+    else dig(x,y+1,len);
+  }else if(togo <= (100-choice_up )/2 + choice_up){
     if(x>0)
     dig(x-1,y,len);
     else
     dig(x,y,len);
-  }else if(togo==1 || togo==2){
-    if(len==0)dig(x,y-1,len);
-    else dig(x,y+1,len);
   }else{
-    if(x<=14)
+    if(x<=width-1)
     dig(x+1,y,len);
     else
     dig(x,y,len);
 
   }
 }
-int main(){
- for(int i=0;i<100;i++){
-   for(int j=0;j<15;j++){
+int ctoi(char c) {
+	if (c >= '0' && c <= '9') {
+		return c - '0';
+	}
+	return 0;
+}
+
+int main(int argc,char* argv[]){
+
+
+ //オプションの受け取り
+ int opt;
+
+ opterr = 0; //getopt()のエラーメッセージを無効にする。
+
+ while ((opt = getopt(argc, argv, "x:y:g:t:r:h")) != -1) {
+    //コマンドライン引数のオプションがなくなるまで繰り返す
+    switch (opt) {
+        case 'x':
+            //printf("-xがオプションとして渡されました\n");
+            width = atoi(optarg);
+            break;
+
+        case 'y':
+            //printf("-yがオプションとして渡されました\n");
+            height = atoi(optarg);
+            break;
+        case 'g':
+            //printf("-gがオプションとして渡されました\n");
+            //printf("引数optarg = %s\n", optarg);
+            choice_up= atoi(optarg);
+            //cout<<choice_up<<endl;
+            break;
+        case 't':
+          thinkTime = atoi(optarg);
+          break;
+        case 'r':
+          repeat = atoi(optarg);
+        case 'h':
+            //printf("-hがオプションとして渡されました\n");
+            printf("-x [argument] argumentの幅 -y [argument] argumentの高さ -g [argument] argumentの確率で上方向を選択 既定値70 \n");
+            break;
+
+        default: /* '?' */
+            //指定していないオプションが渡された場合
+            printf("Usage: %s [-f arg] [-g arg] [-h] arg1 ...\n", argv[0]);
+            break;
+    }
+ }
+ for(int i=0;i<height;i++){
+   for(int j=0;j<width;j++){
      maze[i][j]=1;
    }
  }
@@ -50,33 +105,41 @@ int main(){
  int x0,x1;
  x0=5; x1=9;
 
- rep(i,3)dig(x0,100,0);
- rep(i,3)dig(x1,100,0);
- rep(i,3)dig(x0,0,100);
- rep(i,3)dig(x1,0,100);
+ //cout<<"初期化終了"<<endl;
 
+ rep(i,repeat)dig(x0,height,0);
+ rep(i,repeat)dig(x1,height,0);
+ rep(i,repeat)dig(x0,0,height);
+ rep(i,repeat)dig(x1,0,height);
 
+ //cout<<"生成終了"<<endl;
 
- for(int i=0;i<99;i++){
+ for(int i=0;i<height-1;i++){
    obstacle+='[';
-   for(int j=0;j<14;j++){
+   for(int j=0;j<width-1;j++){
      //cerr<<maze[i][j];
      obstacle+=to_string(maze[i][j]);
      obstacle+=',';
    }
-   obstacle+=to_string(maze[i][14]);
+   obstacle+=to_string(maze[i][width-1]);
    obstacle+="],";
    //cerr<<endl;
  }
  obstacle+='[';
- rep(j,14){
+ rep(j,width-1){
    obstacle+='0';
    obstacle+=',';
  }
  obstacle+='0';
  obstacle+=']';
 
- cout<<"{\"filetype\":\"race course\",\"width\":15,\"length\":100,\"vision\":20,\"thinkTime\":20000,\"stepLimit\":100,\"x0\":5,\"x1\":9,\"obstacles\":";
+ cout<<"{\"filetype\":\"race course\",\"width\":";
+ cout<<width;
+ cout<<",\"length\":";
+ cout<<height;
+ cout<<",\"vision\":20,\"thinkTime\":";
+ cout<<thinkTime;
+ cout<<",\"stepLimit\":100,\"x0\":5,\"x1\":9,\"obstacles\":";
  cout<<"["<<obstacle<<"]}"<<endl;
 
 
