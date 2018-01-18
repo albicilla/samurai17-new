21b6052a47ae7c85eb50030a694836888bb8f9a6
Motonari Tsuzuki <motonari728@gmail.com>
2018-01-19 04:47:30 +0900

    stateの設計まで

--- gymEnv/samuraiEnv.py
+++ gymEnv/samuraiEnv.py
@@ -4,35 +4,36 @@ import gym
 import numpy as np
 import gym.spaces
 
-import map
+# import map
+from builtins import input
 
 
 class MyEnv(gym.Env):
     metadata = {'render.modes': ['human', 'ansi']}
-    FIELD_TYPES = [
-        'S',  # 0: スタート
-        'G',  # 1: ゴール
-        '~',  # 2: 芝生(敵の現れる確率1/10)
-        'w',  # 3: 森(敵の現れる確率1/2)
-        '=',  # 4: 毒沼(1step毎に1のダメージ, 敵の現れる確率1/2)
-        'A',  # 5: 山(歩けない)
-        'Y',  # 6: 勇者
-    ]
-    MAP = np.array([
-        [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],  # "AAAAAAAAAAAA"
-        [5, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],  # "AA~~~~~~~~~~"
-        [5, 5, 2, 0, 2, 2, 5, 2, 2, 4, 2, 2],  # "AA~S~~A~~=~~"
-        [5, 2, 2, 2, 2, 2, 5, 5, 4, 4, 2, 2],  # "A~~~~~AA==~~"
-        [2, 2, 3, 3, 3, 3, 5, 5, 2, 2, 3, 3],  # "~~wwwwAA~~ww"
-        [2, 3, 3, 3, 3, 5, 2, 2, 1, 2, 2, 3],  # "~wwwwA~~G~~w"
-        [2, 2, 2, 2, 2, 2, 4, 4, 2, 2, 2, 2],  # "~~~~~~==~~~~"
-    ])
+        # FIELD_TYPES = [
+        #     'S',  # 0: スタート
+        #     'G',  # 1: ゴール
+        #     '~',  # 2: 芝生(敵の現れる確率1/10)
+        #     'w',  # 3: 森(敵の現れる確率1/2)
+        #     '=',  # 4: 毒沼(1step毎に1のダメージ, 敵の現れる確率1/2)
+        #     'A',  # 5: 山(歩けない)
+        #     'Y',  # 6: 勇者
+        # ]
+        # MAP = np.array([
+        #     [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],  # "AAAAAAAAAAAA"
+        #     [5, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],  # "AA~~~~~~~~~~"
+        #     [5, 5, 2, 0, 2, 2, 5, 2, 2, 4, 2, 2],  # "AA~S~~A~~=~~"
+        #     [5, 2, 2, 2, 2, 2, 5, 5, 4, 4, 2, 2],  # "A~~~~~AA==~~"
+        #     [2, 2, 3, 3, 3, 3, 5, 5, 2, 2, 3, 3],  # "~~wwwwAA~~ww"
+        #     [2, 3, 3, 3, 3, 5, 2, 2, 1, 2, 2, 3],  # "~wwwwA~~G~~w"
+        #     [2, 2, 2, 2, 2, 2, 4, 4, 2, 2, 2, 2],  # "~~~~~~==~~~~"
+        # ])
     MAX_STEPS = 100
 
     def __init__(self):
         super().__init__()
         # action_space, observation_space, reward_range を設定する
-        self.action_space = gym.spaces.Discrete(9)  # 東西南北
+        self.action_space = gym.spaces.Discrete(9) 
         self.observation_space = gym.spaces.Box(
             low=0,
             high=len(self.FIELD_TYPES),
@@ -42,46 +43,84 @@ class MyEnv(gym.Env):
         self._reset()
 
     def _reset(self):
-        # 諸々の変数を初期化する
-        self.pos = self._find_pos('S')[0]
-        self.goal = self._find_pos('G')[0]
+        # map.init()
+        self.total_time = int(self.readline())
+        self.max_step = int(self.readline())
+        width, height = [int(x) for x in self.readline().split()]
+        self.view = int(self.readline())
+        # 初期化終了
+        print(0)
+        self.map = Map(width, height, self.view)
         self.done = False
-        self.damage = 0
-        self.steps = 0
-        return self._observe()
+        return map.observe()
+        # # 諸々の変数を初期化する
+        # self.pos = self._find_pos('S')[0]
+        # self.goal = self._find_pos('G')[0]
+        # self.damage = 0
+        # self.steps = 0
+        # return self._observe()
+
+
+    def readline(self):
+        x = input()
+        print(str(x), file=sys.stderr)
+        return x
+    
+
+    def observe(self):                                
+        # while True:
+        step = int(self.readline())
+        time = int(self.readline())
+        ps = []
+        for j in range(2):
+            xs = [int(x) for x in self.readline().split()]
+            # x,y, vx,vy
+            p = [np.array([xs[0], xs[1]]), np.array([xs[2], xs[3]])]
+            ps.append(tuple(p))
+        map.initPosMap()
+        map.playerMap[ps[0][0][0], ps[0][0][1]] = 1
+        map.enemyMap[ps[1][0][0], ps[1][0][1]] = 1
+        for y in range(ps[0][0][1] - self.view, ps[0][0][1] + self.view + 1, 1):
+            ls = [int(v) for v in self.readline().split()]
+            if y > 0:
+                map.setline(y, ls)
+        return map.observe
+
 
     def _step(self, action):
         # 1ステップ進める処理を記述。戻り値は observation, reward, done(ゲーム終了したか), info(追加の情報の辞書)
-        if action == 0:
-            next_pos = self.pos + [1, 1]
-        elif action == 1:
-            next_pos = self.pos + [0, -1]
-        elif action == 2:
-            next_pos = self.pos + [1, 0]
-        elif action == 3:
-            next_pos = self.pos + [-1, 0]
+        print(action)
+        return map.observe() # TODO
+        # if action == 0:
+        #     next_pos = self.pos + [1, 1]
+        # elif action == 1:
+        #     next_pos = self.pos + [0, -1]
+        # elif action == 2:
+        #     next_pos = self.pos + [1, 0]
+        # elif action == 3:
+        #     next_pos = self.pos + [-1, 0]
 
-        if self._is_movable(next_pos):
-            self.pos = next_pos
-            moved = True
-        else:
-            moved = False
+        # if self._is_movable(next_pos):
+        #     self.pos = next_pos
+        #     moved = True
+        # else:
+        #     moved = False
 
-        observation = self._observe()
-        reward = self._get_reward(self.pos, moved)
-        self.damage += self._get_damage(self.pos)
-        self.done = self._is_done()
-        return observation, reward, self.done, {}
+        # observation = self._observe()
+        # reward = self._get_reward(self.pos, moved)
+        # self.damage += self._get_damage(self.pos)
+        # self.done = self._is_done()
+        # return observation, reward, self.done, {}
 
-    def _render(self, mode='human', close=False):
-        # human の場合はコンソールに出力。ansiの場合は StringIO を返す
-        outfile = StringIO() if mode == 'ansi' else sys.stdout
-        outfile.write('\n'.join(' '.join(
-                self.FIELD_TYPES[elem] for elem in row
-                ) for row in self._observe()
-            ) + '\n'
-        )
-        return outfile
+    # def _render(self, mode='human', close=False):
+    #     # human の場合はコンソールに出力。ansiの場合は StringIO を返す
+    #     outfile = StringIO() if mode == 'ansi' else sys.stdout
+    #     outfile.write('\n'.join(' '.join(
+    #             self.FIELD_TYPES[elem] for elem in row
+    #             ) for row in self._observe()
+    #         ) + '\n'
+    #     )
+    #     return outfile
 
     def _close(self):
         pass
@@ -100,33 +139,6 @@ class MyEnv(gym.Env):
         else:
             return -1
 
-    def _get_damage(self, pos):
-        # ダメージの計算
-        field_type = self.FIELD_TYPES[self.MAP[tuple(pos)]]
-        if field_type == 'S':
-            return 0
-        elif field_type == 'G':
-            return 0
-        elif field_type == '~':
-            return 10 if np.random.random() < 1/10. else 0
-        elif field_type == 'w':
-            return 10 if np.random.random() < 1/2. else 0
-        elif field_type == '=':
-            return 11 if np.random.random() < 1/2. else 1
-
-    def _is_movable(self, pos):
-        # マップの中にいるか、歩けない場所にいないか
-        return (
-            0 <= pos[0] < self.MAP.shape[0]
-            and 0 <= pos[1] < self.MAP.shape[1]
-            and self.FIELD_TYPES[self.MAP[tuple(pos)]] != 'A'
-        )
-
-    def _observe(self):
-        # マップに勇者の位置を重ねて返す
-        observation = self.MAP.copy()
-        observation[tuple(self.pos)] = self.FIELD_TYPES.index('Y')
-        return observation
 
     def _is_done(self):
         # 今回は最大で self.MAX_STEPS までとした
@@ -141,3 +153,43 @@ class MyEnv(gym.Env):
         return np.array(list(zip(*np.where(
         self.MAP == self.FIELD_TYPES.index(field_type)
     ))))
+
+
+class Map:
+    def __init__(self, width, height, view):
+        self.maxw = 20
+        self.maxh = 140
+        self.shape = (self.maxh, self.maxw)
+        self.w = width
+        self.h = height
+        # 障害物マップ
+        self.m = np.zeros(self.shape)
+        # 左寄せ、右壁は障害物にしておく
+        self.m[:, width-1:] = 1
+        # 0: unknown, 1: known
+        self.unknownMap = np.zeros(self.shape)
+        self.unknownMap[:, width-1:] = 1
+        # self.m = [[0] * width for y in range(height + view + 1)]
+        self.playerMap = np.zeros(self.shape)
+        self.enemyMap = np.zeros(self.shape)
+        self.maxy = 0
+
+    def setline(self, y, l):
+        self.m[y, 0:self.w] = l
+        self.unknownMap[y, 0:self.w] = 1
+        self.maxy = max(self.maxy, y)
+
+    def getXY(self, x, y):
+        if x < 0 or x >= self.w or y < 0 or y > self.maxy:
+            return 1
+        return self.m[y][x]
+
+    def getP(self, p):
+        return self.getXY(p[0], p[1])
+
+    def initPosMap(self):
+        self.playerMap = np.zeros(self.shape)
+        self.enemyMap = np.zeros(self.shape)
+
+    def observe(self):
+        return np.vstack(self.m, self.unknownMap, self.playerMap, self.unknownMap)
